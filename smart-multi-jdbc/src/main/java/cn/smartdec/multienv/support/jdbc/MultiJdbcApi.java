9a450782e08c9e73e1f37a250010d5dfab85d126
erwenchan <erwenchan@qq.com>
2021-12-02 15:44:19 +0800

    del

--- smart-multi-jdbc/src/main/java/cn/smartdec/multienv/support/jdbc/MultiJdbcApi.java
+++
@@ -1,9 +0,0 @@
-package cn.smartdec.multienv.support.jdbc;
-
-import cn.smartdec.multienv.data.jdbc.SubDataSourceProperties;
-
-public interface MultiJdbcApi {
-
-    SubDataSourceProperties subDataSourceProperties(String envKey);
-
-}
